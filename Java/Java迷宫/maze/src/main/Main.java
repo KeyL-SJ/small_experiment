a1d9aebab92c7e2ad77e1efc24f6e68c8d9d43c3
KeyL-SJ <78483846+KeyL-SJ@users.noreply.github.com>
2023-05-14 08:28:41 +0800

    Add files via upload

---
+++ Java/Java迷宫/maze/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+
+
+
+//Main类
+public class Main {
+
+	public static void main(String[] args) {
+		GameFrame frame = new GameFrame();
+		GamePanel panel = new GamePanel(frame);
+		frame.add(panel);
+		frame.setVisible(true);
+	}
+}
